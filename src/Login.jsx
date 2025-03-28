f9d49e7945ab04bbf469c3d75cf3c908efa49ace
rockcee31 <thapliyalrohit31@gmail.com>
2025-03-28 18:54:48 +0530

    done making call to login api and allowing cors with credential true like token

--- src/Login.jsx
+++ src/Login.jsx
@@ -10,7 +10,7 @@ try{
     const res = await axios.post("http://localhost:3000/login",{
       emailId,
       password
-    })
+    },{withCredentials:true}) //allow server to send response with credentials like cookiee and send back that cookie for otheer api so that we can authenticate
     console.log(res)
 }catch(err){
   console.log(err)
@@ -18,7 +18,7 @@ try{
 }//when you make api call from x domain to y domain than it gives you cors error(cross origin error and only browser throws this error cause they don't allow it due security reason even the port number matter here if your making call to 7777 from 3000 this also throw error so browser req to be made to same domain and host so we can handle it in api level we can bypass cors in the api level or backend so in login api we can bypass cors so we can use cors middleware provided by exppress 
 // u just have to do npm i cors 
 
-
+// //controlled component first value is set to password means password is controlliing aned when ever we change the value onchange is changing password value
 
   return (
 
@@ -37,14 +37,13 @@ try{
             <div className="validator-hint hidden">Enter valid email address</div>
           </div>
 
-          <div className="validator-hint hidden">Enter valid email address</div>
 
           {/* password */}
           <div>
           <span className="fieldset-legend">Password</span>
             <label className="input validator">
               <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path><circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle></g></svg>
-              <input type="text" required placeholder="Password" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must be more than 8 characters, including number, lowercase letter, uppercase letter" value={password} onChange={(e)=>setpassworrd(e.target.password)}/>
+              <input type="text" required placeholder="Password" minlength="8" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must be more than 8 characters, including number, lowercase letter, uppercase letter" value={password} onChange={(e)=>setpassworrd(e.target.value)}/> 
             </label>
             <p className="validator-hint hidden">
               Must be more than 8 characters, including
@@ -57,7 +56,7 @@ try{
 
 
           <div className="card-actions justify-center">
-            <button className="btn btn-active btn-primary" onClick={handleLogin()}>Login</button>
+            <button className="btn btn-active btn-primary" onClick={handleLogin}>Login</button>
           </div>
           
         </div>
